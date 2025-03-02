6905d5dd532509d4623d1949757ed0988a26ee46
Danilo V Silva <danilovicentindasilva>
2025-03-02 20:21:00 -0300

    Galeria de livros

--- app/books-list/page.tsx
+++ app/books-list/page.tsx
@@ -13,7 +13,7 @@ const books = [
     title: "Os 7 Pilares: A Jornada Além da Montanha",
     description:
       "Uma jornada épica por um mundo de fantasia cheio de desafios e segredos antigos.",
-    image: "/imagens/book1.webp",
+    image: "/imagens/01-capa.webp",
     pages: 350,
   },
   {
@@ -21,14 +21,14 @@ const books = [
     title: "Outro Livro Exemplo",
     description:
       "Uma história envolvente sobre coragem e descobertas inesperadas.",
-    image: "/imagens/book2.webp",
+    image: "/imagens/em-breve.webp",
     pages: 270,
   },
   {
     id: 3,
     title: "Mais um Livro Aqui",
     description: "Aventura, mistério e emoção em um universo incrível.",
-    image: "/imagens/book3.webp",
+    image: "/imagens/em-breve.webp",
     pages: 420,
   },
 ]
@@ -53,7 +53,7 @@ export default function BooksList() {
     <>
       <Header />
       <Nav />
-      <main className="min-h-screen text-gray-900 p-10 ml-40 flex flex-col items-center">
+      <main className="min-h-screen text-gray-900 p-10 ml-40 flex flex-col ">
         {/* Botões de Navegação */}
         <div className="absolute top-5 right-10 flex gap-4">
           <button
@@ -75,31 +75,32 @@ export default function BooksList() {
         <p className="text-lg text-gray-600 mb-6">{selectedBook.description}</p>
 
         {/* Galeria de Livros */}
-        <div className="flex justify-center items-center gap-20 w-full mt-10">
+        <div className="flex gap-20 w-full mt-10">
           {books.map((book, index) => (
-            <motion.div
-              key={book.id}
-              className="cursor-pointer relative transition-transform duration-300 shadow-2xl rounded-lg"
-              onClick={() => setSelectedIndex(index)}
-              animate={{
-                scale: selectedBook.id === book.id ? 1.1 : 1,
-                transition: { duration: 0.3 },
-              }}
-            >
-              {/* Sombra para dar o efeito de flutuação */}
-              <div className="absolute -bottom-4 left-1/2 transform -translate-x-1/2 w-[90%] h-6 bg-black opacity-20 blur-md rounded-full"></div>
+            <div key={book.id}>
+              <motion.div
+                className="cursor-pointer relative transition-transform duration-300 shadow-2xl rounded-lg"
+                onClick={() => setSelectedIndex(index)}
+                animate={{
+                  scale: selectedBook.id === book.id ? 1.1 : 1,
+                  transition: { duration: 0.3 },
+                }}
+              >
+                {/* Sombra para dar o efeito de flutuação */}
+                <div className="absolute -bottom-4 left-1/2 transform -translate-x-1/2 w-[90%] h-6 bg-black opacity-20 blur-md rounded-full"></div>
 
-              <Image
-                src={book.image}
-                alt={book.title}
-                width={300}
-                height={400}
-                className="rounded-lg"
-              />
+                <Image
+                  src={book.image}
+                  alt={book.title}
+                  width={300}
+                  height={400}
+                  className="rounded-lg"
+                />
+              </motion.div>
               <p className="text-center text-gray-800 font-semibold mt-10">
                 {book.title}
               </p>
-            </motion.div>
+            </div>
           ))}
         </div>
 
