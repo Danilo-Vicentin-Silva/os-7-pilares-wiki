7c6b05c334e2099df5927fd2a3c1e681cc5fa7ba
Danilo V Silva <danilovicentindasilva>
2025-03-21 20:12:36 -0300

    1.1

--- app/pages/books/[id]/page.tsx
+++ app/pages/books/[id]/page.tsx
@@ -1,13 +1,19 @@
-import books from "@/app/data/books"
 import { notFound } from "next/navigation"
 import Image from "next/image"
 import Link from "next/link"
 import { CircleArrowLeft, MoveUpRight } from "lucide-react"
 import ShareButton from "@/app/components/ShareButton"
 import FavoriteButton from "@/app/components/favoriteButton"
+import books from "@/app/data/books"
 
-export default function BookPage({ params }: { params: { id: string } }) {
-  const book = books.find((b) => b.id.toString() === params.id)
+// Atualize o tipo para ser compatível com o Next.js
+interface BookPageProps {
+  params: Promise<{ id: string }>
+}
+
+export default async function BookPage({ params }: BookPageProps) {
+  const resolvedParams = await params // Resolva o parâmetro assíncrono
+  const book = books.find((b) => b.id.toString() === resolvedParams.id)
 
   if (!book) {
     return notFound()
@@ -20,7 +26,7 @@ export default function BookPage({ params }: { params: { id: string } }) {
           <CircleArrowLeft size={50} />
         </Link>
       </button>
-      {/* Imagem do Livro (esquerda) */}
+
       <div className="fixed top-[10%] left-[28%] z-50 max-lg:top-0">
         <Image
           src={book.image}
@@ -33,24 +39,13 @@ export default function BookPage({ params }: { params: { id: string } }) {
 
       <div className="flex flex-col fixed top-[45%] left-[48%] z-50 gap-10 w-full max-lg:hidden">
         <div className="flex flex-row items-center justify-between w-[600px]">
-          {/* Botão de adquirir o livro (alinhado à esquerda) */}
           <Link href={`https://a.co/d/btg3mLB`} target="_blank" rel="nofollow">
             <button className="bg-[#402d09] text-white px-6 py-2 rounded-3xl shadow-md hover:bg-[#402d0984] transition">
               Adquira o seu <MoveUpRight className="inline" />
             </button>
           </Link>
 
-          {/* Grupo de botões (alinhados à direita) */}
           <div className="flex flex-row items-center gap-4 max-lg:hidden">
-            {/* <Link
-              href={`https://a.co/d/btg3mLB`}
-              target="_blank"
-              rel="nofollow"
-            >
-              <button className="bg-[#402d09] text-white w-14 h-14 flex items-center justify-center rounded-full shadow-md hover:bg-[#402d0984] transition">
-                <Users className="w-6 h-6" />
-              </button>
-            </Link> */}
             <ShareButton />
             <FavoriteButton />
           </div>
@@ -67,17 +62,13 @@ export default function BookPage({ params }: { params: { id: string } }) {
         <h3 className="text-[#73551f]">{book.description}</h3>
       </div>
 
-      {/* Seção principal do livro */}
       <main className="text-white fixed top-1/3 right-0 p-10 flex items-center justify-center w-full h-[80vh] max-lg:p-0 max-lg:overflow-scroll">
-        {/* Seção do Livro */}
-        <div className="flex bg-[#d9a036] p-6 pt-60 pl-40 pr-40 w-[80%] h-full gap-10 max-lg:w-[100vw] max-lg:p-5   ">
-          {/* Informações do Livro (meio) */}
+        <div className="flex bg-[#d9a036] p-6 pt-60 pl-40 pr-40 w-[80%] h-full gap-10 max-lg:w-[100vw] max-lg:p-5">
           <div className="flex flex-1 flex-col text-justify">
             <h1 className="text-4xl font-bold text-[#402d09] mb-4">Sinopse</h1>
             <p className="text-lg text-white mb-4">{book.largeDescription}</p>
           </div>
 
-          {/* Informações alinhadas à direita */}
           <div className="flex-1 flex flex-col items-end text-right">
             <h1 className="text-4xl font-bold text-[#402d09] mb-4">Editora</h1>
             <p className="mb-5">Publicação Independente na Amazon KDP</p>
